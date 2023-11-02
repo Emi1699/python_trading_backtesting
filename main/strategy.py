9ec637b53ffcd1af4d8201705dd2d8c438519ee7
Emi1699 <emanuelbuliga16@gmail.com>
2023-11-02 19:53:59 +0200

    strategy returns collosal %, lol

--- main/strategy.py
+++ main/strategy.py
@@ -12,8 +12,6 @@ class MyCustomStrategy():
         self.SESSION_HIGH = 0
         self.SESSION_LOW = float('inf')
 
-        candlestick = None
-
         self.stop_loss_pips = 0.00002
         self.reward_risk_ratio = 10
         self.take_profit_pips = self.stop_loss_pips * self.reward_risk_ratio
@@ -21,77 +19,93 @@ class MyCustomStrategy():
         self.session_long_taken = False
         self.session_short_taken = False
 
-        self.running_trades = []
-        self.trades_taken = []
+        self.running_trades = set()
+        self.closed_trades = set()
 
         self.delta_pips = 0
 
-    def next(self):
+    def run_strategy(self):
         for date_time, candlestick in self.data.iterrows():
             current_time = date_time.time() #HHMMSS
 
             # print(candlestick)
             # Capture high and low between 1am and 9am
             if pd.Timestamp('01:00').time() < current_time <= pd.Timestamp('09:00').time():
+                # first close all trades of the previous sessions
+                for trade in self.running_trades:
+                    self.closed_trades.add(trade)
+                    self.running_trades.remove(trade)
+
+
                 self.SESSION_HIGH = max(self.SESSION_HIGH, candlestick["high"])
                 self.SESSION_LOW = min(self.SESSION_LOW, candlestick["low"])
+                self.session_short_taken = False
+                self.session_long_taken = False
 
             # we enter the period when we can take trades
             if current_time > pd.Timestamp('09:00').time():
                 if not self.session_short_taken:  # if we have not already taken the short for the session
                     if candlestick['high'] > self.SESSION_HIGH: # and we surpass the session's high
                         trade = Trade(self.SESSION_HIGH, "short") # take the short for the session
-                        self.running_trades.append(trade)
-                        self.trades_taken.append(trade)
+                        self.running_trades.add(trade)
                         self.session_short_taken = True
-                        print(f'short taken -> {date_time}')
+                        # print(f'short -> {date_time}')
 
                 if not self.session_long_taken: # if we have not already taken the long for the session
                     if candlestick['low'] < self.SESSION_LOW: # and we go below the session's low
                         trade = Trade(self.SESSION_LOW, "long") # take the logn for the session
-                        self.running_trades.append(trade)
-                        self.trades_taken.append(trade)
+                        self.running_trades.add(trade)
                         self.session_long_taken = True
-                        print(f'long taken -> {date_time}')
+                        # print(f'long -> {date_time}')
 
             self.validate_running_trades(candlestick['high'], candlestick['low'])
 
-    def validate_running_trades(self, candlestick_high, candlestick_low):
-        stop_loss_trades = set()
-        take_profit_trades = set()
+        self.calculate_summary()
 
+    def validate_running_trades(self, candlestick_high, candlestick_low):
+        trades_to_close = set()
         for trade in self.running_trades:
             if trade.direction == "short":
-                if self.SESSION_HIGH >= trade.entry_price - self.take_profit_pips:
-                    self.delta_pips += self.take_profit_pips
-                    take_profit_trades.add(trade)
-                elif self.SESSION_HIGH <= trade.entry_price + self.stop_loss_pips:
+                if candlestick_high >= trade.entry_price + self.stop_loss_pips:
                     self.delta_pips -= self.stop_loss_pips
-                    stop_loss_trades.add(trade)
+
+                    self.closed_trades.add(trade)
+
+                    trades_to_close.add(trade)
+
+
+
+                if candlestick_low <= trade.entry_price - self.take_profit_pips:
+                    self.delta_pips += self.take_profit_pips
+
+                    trade.result = "win"
+                    self.closed_trades.add(trade)
+
+                    trades_to_close.add(trade)
 
             if trade.direction == "long":
-                if self.SESSION_LOW < trade.entry_price + self.take_profit_pips:
+                if candlestick_low <= trade.entry_price - self.stop_loss_pips:
                     self.delta_pips -= self.stop_loss_pips
-                    stop_loss_trades.add(trade)
-                elif self.SESSION_HIGH >= trade.entry_price + trade.pips_to_take_profit:
+
+                    self.closed_trades.add(trade)
+
+                    trades_to_close.add(trade)
+
+                if candlestick_high >= trade.entry_price + self.take_profit_pips:
                     self.delta_pips += self.take_profit_pips
-                    take_profit_trades.add(trade)
 
-        for stop_loss_trades in stop_loss_trades:
-            self.running_trades.remove(stop_loss_trades)
+                    trade.result = "win"
+                    self.closed_trades.add(trade)
 
-        for trade_to_take_profit in take_profit_trades:
-            self.running_trades.remove(trade_to_take_profit)
-            # Reset session flags if necessary
-            if trade_to_take_profit.direction == "long":
-                self.session_long_taken = False
-            else:
-                self.session_short_taken = False
-            # Record the trade details in trade history
+                    trades_to_close.add(trade)
+
+        for trade in trades_to_close:
+            self.running_trades.remove(trade)
 
     def calculate_summary(self):
-        total_trades = len(self.trades_taken)
-        winning_trades = len([trade for trade in self.trades_taken if trade.result == 'profit'])
+        self.delta_pips = self.delta_pips * 10000
+        total_trades = len(self.closed_trades)
+        winning_trades = len([trade for trade in self.closed_trades if trade.result == 'win'])
         losing_trades = total_trades - winning_trades
         win_rate = winning_trades / total_trades * 100 if total_trades > 0 else 0
 
@@ -103,12 +117,12 @@ class MyCustomStrategy():
 
 
 
-data = CSVData("eurusd", 2022, 8).load()
+data = CSVData("eurusd", 2022, "all").load()
 print(len(data))
 
 my_strategy = MyCustomStrategy(data)
 
-my_strategy.next()
+my_strategy.run_strategy()
 
 
 # data.columns = ['Open', 'High', 'Low', 'Close']
